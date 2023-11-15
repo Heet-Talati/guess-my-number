e7c8d9efe3a4bd67cb122c97539ce75835426628
Heet-Talati <theheettalati@gmail.com>
2023-11-15 12:50:36 +0530

    score checking

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+// 'use strict';
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Not a number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = '🎉 Correct number';
+    score++;
+  } else if (guess > number) {
+    document.querySelector('.message').textContent = '📈Too high!';
+    score--;
+  } else if (guess < number) {
+    document.querySelector('.message').textContent = '📉Too low!';
+    score--;
+  }
+  console.log(score);
+  document.querySelector('.score').textContent = score;
+});
